aafe9e1cfa277b18ad65631baa6dd370ea621f6e
itspauv <p.viehmann01@gmail.com>
2025-05-24 15:05:27 +0200

    feat: add sidebar trigger

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function RootLayout({
     children,
@@ -8,7 +9,10 @@ export default function RootLayout({
     return (
         <>
             <AppSidebar />
-            <main className="w-full">{children}</main>
+            <main className="w-full relative">
+                <SidebarTrigger className="absolute top-2 left-2 text-foreground" />
+                {children}
+            </main>
         </>
     );
 }
