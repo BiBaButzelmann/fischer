46c7d1c9c6dc1830dd743c30bd8e1c65a1630552
Arne Alpers <arne.alpers@gmail.com>
2025-06-10 19:41:28 +0200

    feat: remove unnecessary schema entries, add phone number instead

---
+++ drizzle/0024_cooing_starbolt.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "profile" DROP COLUMN "co_line";--> statement-breakpoint
+ALTER TABLE "profile" DROP COLUMN "street";--> statement-breakpoint
+ALTER TABLE "profile" DROP COLUMN "city";--> statement-breakpoint
+ALTER TABLE "profile" DROP COLUMN "postal_code";--> statement-breakpoint
+ALTER TABLE "profile" DROP COLUMN "participant_id";
--- src/db/schema/profile.ts
+++ src/db/schema/profile.ts
@@ -1,20 +1,12 @@
 import { relations } from "drizzle-orm";
 import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { user } from "./auth";
-import { participant } from "./participant";
 
 export const profile = pgTable("profile", {
   id: integer("id").generatedAlwaysAsIdentity().primaryKey(),
   name: text("name").notNull(),
-
-  coLine: text("co_line"),
-  street: text("street"),
-  city: text("city"),
-  postalCode: text("postal_code"),
-
   userId: text("user_id").notNull(),
-  participantId: integer("participant_id"),
-
+  phoneNumber: text("phone_number").notNull(),
   createdAt: timestamp("created_at")
     .$defaultFn(() => /* @__PURE__ */ new Date())
     .notNull(),
@@ -27,9 +19,5 @@ export const profileRelations = relations(profile, ({ one }) => ({
   user: one(user, {
     fields: [profile.userId],
     references: [user.id],
-  }),
-  participant: one(participant, {
-    fields: [profile.participantId],
-    references: [participant.id],
   }),
 }));
