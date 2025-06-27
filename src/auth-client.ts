3fa58cd316f5f3544209bda75423f905b0e05b75
itspauv <p.viehmann01@gmail.com>
2025-06-27 18:15:44 +0200

    fix: remove base url

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -3,7 +3,6 @@ import { adminClient } from "better-auth/client/plugins";
 import { ac, admin, user } from "./permissions";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
   plugins: [
     adminClient({
       ac,
