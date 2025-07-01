104e6372de40c616383aef78ac136a630a1b3cf6
Arne <arne.alpers@gmail.com>
2025-07-01 12:11:34 +0200

    fix: remove unused imports

--- src/components/anmeldung/forms/participate-form.tsx
+++ src/components/anmeldung/forms/participate-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useTransition } from "react";
+import { useTransition } from "react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 import {
