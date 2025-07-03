1e3b564e970ed7e76a5d61ae4bcd913a94c9a804
itspauv <p.viehmann01@gmail.com>
2025-07-03 10:04:46 +0200

    feat: add auth to server action

--- src/actions/pgn.ts
+++ src/actions/pgn.ts
@@ -1,9 +1,18 @@
 "use server";
 
+import { authWithRedirect } from "@/auth/utils";
 import { db } from "@/db/client";
+import { isUserParticipantInGame } from "@/db/repositories/game";
 import { pgn } from "@/db/schema/pgn";
 
 export const savePGN = async (newValue: string, gameId: number) => {
+  const session = await authWithRedirect();
+
+  // TODO: check for referee permissions
+  if ((await isUserParticipantInGame(gameId, session.user.id)) === false) {
+    throw new Error("You are not a participant in this game.");
+  }
+
   await db
     .insert(pgn)
     .values({ gameId, value: newValue })
