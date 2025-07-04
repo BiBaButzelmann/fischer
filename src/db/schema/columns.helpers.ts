78a5ef7cf4dfc4eea5c5ce5acf206a2532fc1d9d
itspauv <p.viehmann01@gmail.com>
2025-07-04 18:11:55 +0200

    refactor: move game results enum to columns helpers

--- src/db/schema/columns.helpers.ts
+++ src/db/schema/columns.helpers.ts
@@ -12,3 +12,12 @@ export const timestamps = {
 export const availableMatchDays = ["tuesday", "thursday", "friday"] as const;
 
 export const matchDay = pgEnum("match_day", availableMatchDays);
+
+export const gameResults = [
+  "draw",
+  "white_wins",
+  "black_wins",
+  // TODO: nicht angetreten etc.
+] as const;
+
+export const gameResult = pgEnum("result", gameResults);
