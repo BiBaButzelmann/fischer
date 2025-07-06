ef3b6a6b8e8a5a22bce293bf1a9cc5a927c493fa
itspauv <p.viehmann01@gmail.com>
2025-07-06 20:17:34 +0200

    feat: add footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/sidebar";
+import { Separator } from "@/components/ui/separator";
 // import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function RootLayout({
@@ -9,9 +10,18 @@ export default function RootLayout({
   return (
     <>
       <AppSidebar />
-      <main className="w-full relative">
+      <main className="w-full relative ">
         {/* <SidebarTrigger className="absolute top-2 left-2 text-foreground" /> */}
-        <div className="px-4 py-4 max-w-6xl mx-auto">{children}</div>
+        <div className="min-h-screen px-4 py-4 max-w-6xl mx-auto">
+          {children}
+        </div>
+        <footer>
+          <Separator />
+          <div className="text-center p-6 text-xs text-muted-foreground">
+            <p>© 2025 HSK Klubturnier</p>
+            <p>Kontakt: klubturnier@hsk1830.de | Tel: +49 123 456789</p>
+          </div>
+        </footer>
       </main>
     </>
   );
