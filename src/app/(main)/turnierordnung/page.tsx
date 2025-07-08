70f7fc4e2b19958cd9bebde963aa1f743efb410a
Arne <arne.alpers@gmail.com>
2025-07-08 14:26:05 +0200

    feat: add turnierordnung page

---
+++ src/app/(main)/turnierordnung/page.tsx
@@ -0,0 +1,9 @@
+export default function Turnierordnung() {
+  return (
+    <iframe
+      src="/pdfs/turnierordnung.pdf"
+      className="w-full h-full"
+      style={{ border: "none" }}
+    />
+  );
+}
