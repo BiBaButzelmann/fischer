e8ee8b23eec45691861e2e9792d423b2bb39de5f
itspauv <p.viehmann01@gmail.com>
2025-07-10 21:11:17 +0200

    fix: update file names + update styling

--- src/app/(full-page)/ausschreibung/page.tsx
+++ src/app/(full-page)/ausschreibung/page.tsx
@@ -2,7 +2,7 @@ export default function Ausschreibung() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="/pdfs/ausschreibung_25.pdf"
+        src="/pdfs/ausschreibung.pdf"
         className="w-full h-full self-center"
         style={{ border: "none" }}
       />
