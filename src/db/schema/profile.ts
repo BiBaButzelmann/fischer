00ebe0e3544454ea41428456cf0153f47c884ce4
Arne <arne.alpers@gmail.com>
2025-07-12 17:59:05 +0200

    fix: remove participant_id

--- src/db/schema/profile.ts
+++ src/db/schema/profile.ts
@@ -1,7 +1,6 @@
 import { relations } from "drizzle-orm";
 import { integer, pgTable, text } from "drizzle-orm/pg-core";
 import { user } from "./auth";
-import { participant } from "./participant";
 import { timestamps } from "./columns.helpers";
 
 export const profile = pgTable("profile", {
@@ -12,7 +11,6 @@ export const profile = pgTable("profile", {
   phoneNumber: text("phone_number"),
 
   userId: text("user_id").notNull(),
-  participantId: integer("participant_id"),
 
   ...timestamps,
 });
@@ -21,9 +19,5 @@ export const profileRelations = relations(profile, ({ one }) => ({
   user: one(user, {
     fields: [profile.userId],
     references: [user.id],
-  }),
-  participant: one(participant, {
-    fields: [profile.participantId],
-    references: [participant.id],
   }),
 }));
