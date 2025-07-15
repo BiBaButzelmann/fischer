5f4656d72cfb8c05b9a7eb3addbd1f6b0e12da24
Arne <arne.alpers@gmail.com>
2025-07-15 14:54:59 +0200

    fix: url

--- src/app/(full-page)/ausschreibung/page.tsx
+++ src/app/(full-page)/ausschreibung/page.tsx
@@ -2,7 +2,7 @@ export default function Ausschreibung() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="http://docs.google.com/gview?url=https://klubturnier.hsk1830.de/pdfs/ausschreibung.pdf&embedded=true"
+        src="https://docs.google.com/gview?url=https://klubturnier.hsk1830.de/pdfs/ausschreibung.pdf&embedded=true"
         className="w-full h-full self-center"
         style={{ border: "none" }}
       />
