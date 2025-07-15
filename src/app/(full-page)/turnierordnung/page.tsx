844905d6737f84fb9bc1b1f05501c212fd57951a
Arne <arne.alpers@gmail.com>
2025-07-15 14:36:00 +0200

    fix: change url

--- src/app/(full-page)/turnierordnung/page.tsx
+++ src/app/(full-page)/turnierordnung/page.tsx
@@ -2,7 +2,7 @@ export default function Turnierordnung() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="http://docs.google.com/gview?url=https://your-domain.com/pdfs/turnierordnung.pdf&embedded=true"
+        src="http://docs.google.com/gview?url=https://klubturnier.hsk1830.de/pdfs/turnierordnung.pdf&embedded=true"
         className="w-full h-full self-center"
         style={{ border: "none" }}
       />
