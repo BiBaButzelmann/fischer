9e3e4a23fe428788ffdff34c50a0d28e23e13c7b
Arne <arne.alpers@gmail.com>
2025-08-02 21:01:55 +0200

    fix: add dependencies

--- src/components/calendar/my-games-calendar.tsx
+++ src/components/calendar/my-games-calendar.tsx
@@ -85,7 +85,7 @@ export function MyGamesCalendar({ events, matchdays = [] }: Props) {
         }
       });
     },
-    [matchdays, validDropDates],
+    [matchdays, validDropDates, isSameDate],
   );
 
   const handleEventClick = useCallback(
