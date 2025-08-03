0579b9ff43adde0419c148739cfc73df4bed0beb
Arne <arne.alpers@gmail.com>
2025-08-03 13:47:19 +0200

    linting

--- src/app/(main)/partien/page.tsx
+++ src/app/(main)/partien/page.tsx
@@ -84,10 +84,6 @@ export default async function Page({
     Number(selectedTournamentId),
   );
 
-  const selectedMatchday = matchdayId
-    ? matchdays.find((md) => md.id === Number(matchdayId))
-    : null;
-
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900 mb-4">Partien</h1>
