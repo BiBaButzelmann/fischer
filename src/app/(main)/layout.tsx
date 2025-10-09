cdfafad2395772e15dfd8bd74eb9007c6f325d15
Arne Alpers <aalpers@stern-wywiol-gruppe.de>
2025-10-09 11:15:00 +0200

    feat: adjust padding for mobile on global layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <>
       <AppSidebarWrapper />
       <main className="w-full">
-        <div className="flex items-center justify-between px-4 md:px-10 py-4">
+        <div className="flex items-center justify-between px-4 md:px-10 pt-3">
           <SidebarTrigger className="hover:bg-secondary hover:text-current" />
           <NotificationCenter />
         </div>
