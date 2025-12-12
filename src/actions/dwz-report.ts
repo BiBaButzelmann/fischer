ca2cb97da3c5e0833eef6f411a8e26e6d7043093
not-paul-v <p.viehmann01@gmail.com>
2025-12-12 18:44:27 +0100

    fix: use correct name format

--- src/actions/dwz-report.ts
+++ src/actions/dwz-report.ts
@@ -167,7 +167,7 @@ export const generateDwzReportFile = action(async (groupId: number) => {
     },
   );
 
-  const fileName = `DWZ_Export_Group_${groupId}.txt`;
+  const fileName = `Eloref_${data.groupName}-Gruppe.txt`;
 
   const reportWithoutNonAscii = dwzReport.replace(/[^\x00-\x7F]/g, "");
 
